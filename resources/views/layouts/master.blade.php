c2d73c03c233c302a7fc3688c30a480aec9639a7
andela-sakande <surajudeen.akande@andela.com>
2016-03-03 10:15:31 +0000

    [feature #114941011]Add Project layouts

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+ _____   ___  ___
+|     | /   \/   \  ___ __
+`__ \|  O    O  || . \\ \/
+|___/ \___/\___/ |  _//\_\
+                 |_|
+            https://500px.com
+
+Want to make this site better?  https://tesjil.com/jobs
+-->
+<html>
+<head>
+  <title>@yield('title')</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="_token" content="{{ csrf_token() }}" />
+  <meta name="description" content="Directory And Listing System">
+  <meta name="keywords" content="Laravel, PHP, Nigeria, People, Find">
+  <meta name="author" content="Surajudeen Akande">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+  <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+</head>
+<body>
+
+</body>
+</html>
