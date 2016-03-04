ee016e1eaa597af38083c710cf23b41ccf826c5a
andela-sakande <surajudeen.akande@andela.com>
2016-03-04 15:31:01 +0000

    [Feature #114941011] Remove closure from routes file

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -4,5 +4,8 @@ namespace App\Http\Controllers;
 
 class PagesController extends Controller
 {
-    //
+    public function welcome()
+    {
+        return view('welcome');
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,15 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layouts.master')
+@section('title', 'Tesjil | A verified peopled directory!')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
-
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Tesjil</div>
-                <p>A verified peopled directory!</p>
-            </div>
+@section('custom-css')
+<link rel="stylesheet" type="text/css" href="css/welcome.css">
+@endsection
+@section('content')
+    <div class="container">
+        <div class="content">
+            <img src="http://res.cloudinary.com/dqmv6brld/image/upload/v1457105255/DeathtoStock_Wired3_lblsrf.jpg" class="img-responsive">
+            <div class="title">Tesjil</div>
+            <p>A verified peopled directory!</p>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
