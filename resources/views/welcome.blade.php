e819ee9cae9a8d0b7821f35d22aeaff36d88a8d0
andela-sakande <surajudeen.akande@andela.com>
2016-03-10 22:01:41 +0000

    Change Bootstrap to Darkly Theme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
 <link rel="stylesheet" type="text/css" href="css/welcome.css">
 @endsection
 @section('content')
+
     <div class="container">
         <div class="content">
             <img src="http://res.cloudinary.com/dqmv6brld/image/upload/v1457105255/DeathtoStock_Wired3_lblsrf.jpg" class="img-responsive">
