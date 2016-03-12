504fd6b4955c3f28548339abf7c54ccbda255151
andela-sakande <surajudeen.akande@andela.com>
2016-03-12 18:15:32 +0000

    (feature)Landing page header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,16 @@
 <link rel="stylesheet" type="text/css" href="css/welcome.css">
 @endsection
 @section('content')
+<header>
 <div class="container">
-<header style="width: 100%">
-  <div class="wide">
-    <img src="http://res.cloudinary.com/dqmv6brld/image/upload/v1457105255/DeathtoStock_Wired3_lblsrf.jpg" class="img-responsive img-center" style="width: 1700px">
+  <div class="intro-text">
+  <div class="intro-lead-in">
+
   </div>
+  </div>
+</div>
 </header>
   <div class="title">Tesjil</div>
   <p>A verified peopled directory!</p>
-</div>
+
 @endsection
