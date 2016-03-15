cfb0da261fce50f3a158245fa7cc7591c6b06e6e
andela-sakande <surajudeen.akande@andela.com>
2016-03-15 22:24:13 +0000

    Add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="footer-social">
+        <ul class="social">
+            <li> <a href="#"> <i class=" fa fa-facebook"></i> </a> </li>
+            <li> <a href="#"> <i class="fa fa-twitter"></i> </a> </li>
+            <li> <a href="#"> <i class="fa fa-google-plus"></i> </a> </li>
+            <li> <a href="#"> <i class="fa fa-pinterest"></i> </a> </li>
+            <li> <a href="#"> <i class="fa fa-linkedin"></i> </a> </li>
+        </ul>
+    </div>
+    <div class="footer-footer">
+        <p>Copyright &copy; 2016 Sirolad Inc. </p>
+        <p>Proudly powered by Andela and Laravel.</p>
+    </div>
+</footer>
